a449e4898d726d1804c017ee552fc5820e675f76
akumawu <akumaru.senju@gmail.com>
2022-10-19 23:49:51 +0500

    little change on os-name

--- src/main.py
+++ src/main.py
@@ -5,8 +5,9 @@ import os
 
 
 gpu_unix_command = "lspci | grep VGA"
-
+os_unix_command = "cat /etc/os-release | grep NAME"
 cpu_unix_command = "cat /proc/cpuinfo | grep name"
+
 
 memory_unix_command = {
     "total": "free -m | grep Mem | awk '{print $2}'",
@@ -17,6 +18,8 @@ disk_unix_command = {
     "total": "df -h / | tail -n -1 | awk '{ print $2 \"\t\" }'",
     "used": "df -h / | tail -n -1 | awk '{ print $3 \"\t\" }'"
 }
+
+os_name = os.popen(os_unix_command).readlines()[0].split('"')[1]
 
 gpu_info = os.popen(gpu_unix_command).readlines()
 cpu_info = os.popen(cpu_unix_command).read().split('\n')
@@ -38,9 +41,8 @@ for num,i in enumerate(gpu_info):
         gpus += f"GPU {num}" + str(i.split("controller")[1])
         gpus += "        "
     else:
-        gpus += f"GPU " + str(i.split("controller:")[1]) + '        '
+        gpus += f"GPU :" + str(i.split("controller:")[1]) + '        '
 
-os_name = platform.node()
 kernel = platform.release()
 
 fetch = f"""        OS: {os_name}
